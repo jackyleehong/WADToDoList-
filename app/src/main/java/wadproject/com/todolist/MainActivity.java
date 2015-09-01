d7286440cf64443a7584d2e17ecc956037a0a49a
cheehung89 <cheehung0110@gmail.com>
2015-09-01 20:13:14 +0800

    Edited with some ViewList
    i not sure the code can work or not because my laptop cant run the
    emulator... can some one try help me run and see got viewlist occur or
    not?

--- app/src/main/java/wadproject/com/todolist/MainActivity.java
+++ app/src/main/java/wadproject/com/todolist/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
+ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +28,36 @@ public class MainActivity extends Activity {
                 startActivity(addTask);
             }
         });
+		
+			// Get ListView object from xml
+            listView = (ListView) findViewById(R.id.list);
+            
+            // Defined Array values to show in ListView
+            String[] values = new String[] { "Android List View", 
+                                             "Adapter implementation",
+                                             "Simple List View In Android",
+                                             "Create List View Android", 
+                                             "Android Example", 
+                                             "List View Source Code", 
+                                             "List View Array Adapter", 
+                                             "Android Example List View" 
+                                            };
+    
+            // Define a new Adapter
+            // First parameter - Context
+            // Second parameter - Layout for the row
+            // Third parameter - ID of the TextView to which the data is written
+            // Forth - the Array of data
+    
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+              android.R.layout.simple_list_item_1, values);
+    
+    
+            // Assign adapter to ListView
+            listView.setAdapter(adapter);
+		
+		
+		
     }
 
     @Override
